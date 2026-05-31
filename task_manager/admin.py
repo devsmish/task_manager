88f0cf4b71a0f89ad0fc502b4618a23c1684a531
devsmish <smyshynskyi@gmail.com>
2026-05-31 23:17:11 +0200

    create and registration in admin.py inline form for Task

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from task_manager.models import Task, SubTask, Category
 
 
+class TaskInline(admin.StackedInline):
+    model = SubTask
+    extra = 1
+    max_num = 5
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -10,6 +16,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    inlines = [TaskInline]
     list_display = ('title', 'description', 'status', 'deadline', 'created_at')
     list_filter = ('status', 'categories', 'deadline', 'created_at')
     search_fields = ('title', 'description')
