a06574565c24dc1260ca769aede48dae0a01aff1
devsmish <smyshynskyi@gmail.com>
2026-06-14 14:37:22 +0200

    implement SubTaskCreateSerializer with read_only created_at Closed #22

--- task_manager/serializers.py
+++ task_manager/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from task_manager.models import Task
+from task_manager.models import Task, SubTask
 
 class TaskCreateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,11 @@ class TaskListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'status', 'deadline', 'created_at']
+
+
+class SubTaskCreateSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = SubTask
+        fields = ['id', 'title', 'description', 'task', 'status', 'deadline', 'created_at']
