99fd47fd5681ffe35fe2142645ef59a46d6e0c91
devsmish <smyshynskyi@gmail.com>
2026-07-03 22:51:50 +0200

    feat: implement secure global cursor pagination with 6 items limit Closed #48

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'task_manager.pagination.CustomCursorPagination',
+    'PAGE_SIZE': 6,
+}
