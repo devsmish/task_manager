3e05e3214d21df8aad4913cc750fc4f50262f39f
devsmish <smyshynskyi@gmail.com>
2026-07-07 23:34:49 +0200

    docs: integrate drf-spectacular for OpenAPI 3.0 and Swagger UI Closed #63

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework.routers import DefaultRouter
 
 from rest_framework_simplejwt.views import (
@@ -20,6 +21,11 @@ router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
